df1dab9d013405a99f770bf00d38160194f0b98a
zhangjianghong <Wenwen.8023>
2021-05-04 19:28:01 +0800

    第二版

--- go.mod
+++ go.mod
@@ -5,6 +5,7 @@ go 1.16
 require (
 	github.com/golang/protobuf v1.5.2 // indirect
 	github.com/grpc-ecosystem/grpc-gateway/v2 v2.4.0 // indirect
+	google.golang.org/genproto v0.0.0-20210426193834-eac7f76ac494 // indirect
 	google.golang.org/grpc v1.37.0
 	google.golang.org/grpc/cmd/protoc-gen-go-grpc v1.1.0 // indirect
 	google.golang.org/protobuf v1.26.0 // indirect
--- server/main.go
+++ server/main.go
@@ -1,16 +1,22 @@
 package main
 
 import (
+	"context"
 	trippb "coolcar/proto/gen/go"
 	trip "coolcar/server/tripservice"
 	"log"
 	"net"
+	"net/http"
 
+	"github.com/grpc-ecosystem/grpc-gateway/v2/runtime"
 	"google.golang.org/grpc"
 )
 
+const endpoints = "localhost:8081"
+
 func main() {
 	log.SetFlags(log.Lshortfile)
+	go startGRPCGateway()
 	listen, err := net.Listen("tcp", ":8081")
 	if err != nil {
 		log.Fatalf("failed to listen :%v", err)
@@ -23,4 +29,20 @@ func main() {
 	if err != nil {
 		log.Fatalf("failed to listen :%v", err)
 	}
+}
+
+func startGRPCGateway() {
+	c := context.Background()
+	c, cancel := context.WithCancel(c)
+	defer cancel()
+
+	mux := runtime.NewServeMux()
+
+	err := trippb.RegisterTripServiceHandlerFromEndpoint(
+		c, mux, endpoints, []grpc.DialOption{grpc.WithInsecure()},
+	)
+	if err != nil {
+		log.Fatal(err)
+	}
+	http.ListenAndServe(":8080", mux)
 }
